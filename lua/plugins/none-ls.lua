6dc9c5ae935e0406535715e6fe6c8bf9161f25d3
Your Name <your-email@example.com>
2026-04-30 12:20:46 +0300

    chore(nvim): clean stale template config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,13 +6,12 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
-    -- opts variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    config.sources = {
+    opts.sources = {
       -- Set a formatter
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.prettier,
@@ -28,6 +27,6 @@ return {
         extra_args = { "-i", "2" },
       },
     }
-    return config -- return final config table
+    return opts
   end,
 }
